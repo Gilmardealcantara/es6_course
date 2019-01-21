37b73802a2d0b68ceba083d3ab948f092aa784e6
Gilmar Alcantara <gilmaralcantara@MacBook-Pro-de-Gilmar.local>
2019-01-21 00:16:34 -0200

    update data expression test

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -7,7 +7,7 @@ class DateHelper {
     }
 
     static text2Date(text) {
-        if(!/\d{4}-\d{2}-\d{2}/.test(text))
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(text))
             throw new Error('Date Format invalid - Use YYYY-MM-DD');
         return new Date(...text.split('-').map((v, i) =>  v - i%2))
     }
